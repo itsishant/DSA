ed19889d33bd3b2e38c74fa3f92a6fbdc9f983ff
itsishant <modakmania@gmail.com>
2025-08-28 23:38:46 +0530

    add longest subsequence

--- Arrays/level-3/longestSequence.java
+++ Arrays/level-3/longestSequence.java
@@ -1,9 +1,39 @@
-import java.util.*; 
+import java.util.*;
 
 public class longestSequence {
+
+    public static int Brute (int arr[], int n ){
+        int copy;
+        int ans = -1;
+
+        for(int i = 0; i<n; i++){
+            int a = arr[i];
+            int cnt = 0;
+            
+            for(int j = i+1; j<n; j++){
+                a++;
+                copy = a;
+
+                if( arr[j] == copy){
+                    cnt ++;
+                }
+
+                ans = Math.max(cnt, ans);
+            }
+        }
+        return ans;
+
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
         int n = sc.nextInt();
         int arr[] = new int[n];
+
+        for(int i = 0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println(Brute(arr, n));
+
     }
 }
